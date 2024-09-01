b20875697b9d85c029089e179e607108224a088d
Abhijit Mondal <amondal@microsoft.com>
2024-09-01 10:21:26 +0530

    qr

--- src/eigenvalues.rs
+++ src/eigenvalues.rs
@@ -1,7 +1,6 @@
 #![allow(dead_code)]
 use crate::copy::copy;
 use crate::transpose::transpose;
-use crate::solve_linear::solve;
 use crate::matrix_multiplication_simd::matrix_multiply_simd;
 use crate::qr_decomposition::qr_decomposition;
 use rand_distr::{Distribution, Normal};
@@ -25,7 +24,7 @@ pub fn eigenvalues(a:&[f64], n:usize) -> Vec<f64> {
 
         for i in 0..n {
             b[i*n+i] += s;
-            if (b[i*n+i]-eig[i])/b[i*n+i] > 0.001 {
+            if (b[i*n+i]-eig[i]).abs()/b[i*n+i] > 0.01 {
                 flag = false;
             }
             eig[i] = b[i*n+i];
@@ -39,27 +38,43 @@ pub fn eigenvalues(a:&[f64], n:usize) -> Vec<f64> {
     return eig;
 }
 
-pub fn eigenvectors(a:&[f64], eigvals:&[f64], n:usize)  -> Vec<f64> {
+pub fn eigenvectors(a:&[f64], n:usize)  -> (Vec<f64>, Vec<f64>) {
     let mut eigvec = vec![0.0;n*n];
+    let mut eigval = vec![0.0;n];
 
-    for j in 0..n {
-        let mut b = vec![0.0;n*n];
-        copy(&a, &mut b, n*n);
-        
-        for i in 0..n {
-            b[i*n+i] -= eigvals[j];
-        }
+    let mut b = a.to_owned();
 
-        let y = solve(&b, &vec![0.0;n], n);
-        copy(&y, &mut eigvec[j*n..(j+1)*n], n);
+    for i in 0..n {
+        eigvec[i*n+i] = 1.0;
     }
 
-    return eigvec;
+    loop {
+        let qr = qr_decomposition(&b, n, n);
+        eigvec = matrix_multiply_simd(&eigvec, &qr.0, n, n, n);
+
+        let x = matrix_multiply_simd(&qr.1, &qr.0, n, n, n);
+        copy(&x, &mut b, n*n);
+
+        let mut flag = true;
+
+        for i in 0..n {
+            if (b[i*n+i]-eigval[i]).abs()/b[i*n+i] > 0.01 {
+                flag = false;
+            }
+            eigval[i] = b[i*n+i];
+        }
+
+        if flag {
+            break;
+        }
+    }
+
+    return (eigval, eigvec);
 }
 
 pub fn run() {
-    let n = 3;
-    let m = 3;
+    let n = 50;
+    let m = 50;
 
     let mut rng = thread_rng();
     let normal:Normal<f64> = Normal::new(0.0, 1.0).ok().unwrap();
@@ -72,9 +87,10 @@ pub fn run() {
 
     let b = matrix_multiply_simd(&a, &transpose(&a, n, m), n, m, n);
     let eig = eigenvalues(&b, n);
-    let vecs = eigenvectors(&b, &eig, n);
+    let vecs = eigenvectors(&b, n);
 
     println!("{:?}", b);
     println!("{:?}", eig);
-    println!("{:?}", vecs);
+    println!("{:?}", vecs.0);
+    println!("{:?}", vecs.1);
 }
