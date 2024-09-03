09989a1f72528c106215ef928fa3f6d937a41bb4
Abhijit Mondal <amondal@microsoft.com>
2024-09-03 10:47:51 +0530

    changes

--- src/svd.rs
+++ src/svd.rs
@@ -11,9 +11,7 @@ pub fn svd(a:&[f64], n:usize, m:usize) -> (Vec<f64>, Vec<f64>, Vec<f64>) {
     let a_t = transpose(&a, n, m);
 
     let b = matrix_multiply_simd(&a, &a_t, n, m, n);
-    let c = matrix_multiply_simd(&a_t, &a, m, n, m);
     let b_eig = eigenvectors(&b, n);
-    let c_eig = eigenvectors(&c, m);
 
     let eigvals = b_eig.0;
     let mut s = vec![0.0;n*m];
@@ -23,14 +21,22 @@ pub fn svd(a:&[f64], n:usize, m:usize) -> (Vec<f64>, Vec<f64>, Vec<f64>) {
     }
 
     let u = b_eig.1;
-    let v = c_eig.1;
+    let v1 = matrix_multiply_simd(&a_t, &u, m, n, n);
+
+    let mut s_inv = vec![0.0;n*m];
+
+    for i in 0..r {
+        s_inv[i*m+i] = 1.0/s[i*m+i];
+    }
+
+    let v = matrix_multiply_simd(&v1, &s_inv, m, n, m);
 
     return (u, s, transpose(&v, m, m));
 }
 
 pub fn run() {
-    let n = 3;
-    let m = 3;
+    let n = 30;
+    let m = 50;
 
     let mut rng = thread_rng();
     let normal:Normal<f64> = Normal::new(0.0, 1.0).ok().unwrap();
@@ -43,13 +49,12 @@ pub fn run() {
 
     let res = svd(&a, n, m);
 
-    println!("{:?}", a);
-    println!("{:?}", res.0);
-    println!("{:?}", res.1);
-    println!("{:?}", res.2);
-
     let p = matrix_multiply_simd(&res.0, &res.1, n, n, m);
     let q = matrix_multiply_simd(&p, &res.2, n, m, m);
 
-    println!("{:?}", q);
+    for i in 0..n*m {
+        if (a[i]-q[i]).abs()/a[i] > 0.01  {
+            println!("{:?}, {:?}, {:?}", i, a[i], q[i]);
+        }
+    }
 }
